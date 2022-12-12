bfd510fd58251cb22e53d6a98db522845979568b
Hugo Klepsch <hugo.klepsch@gmail.com>
2022-12-12 08:04:02 -0500

    Solution for day-11/p2

--- cmd/day-11/monkey/mtest.go
+++ cmd/day-11/monkey/mtest.go
@@ -1,19 +1,23 @@
 package monkey
 
 type MonkeyTest interface {
-	Route(i Item) int	
+	Route(i Item) int
+	Divisor() int
 }
 
 type DivisibleMonkeyTest struct {
-	divisibleBy int
-	trueTargetMonkeyNum int
+	divisibleBy          int
+	trueTargetMonkeyNum  int
 	falseTargetMonkeyNum int
 }
 
 func (d DivisibleMonkeyTest) Route(i Item) int {
-	if i % Item(d.divisibleBy) == 0 {
+	if i%Item(d.divisibleBy) == 0 {
 		return d.trueTargetMonkeyNum
 	} else {
 		return d.falseTargetMonkeyNum
 	}
+}
+func (d DivisibleMonkeyTest) Divisor() int {
+	return d.divisibleBy
 }
